1f10275147ec6bf5140e26713bb5032227173016
aidadenisa <aida.denisa@yahoo.com>
2018-02-14 23:36:08 +0200

    Added Spring Boot starting point

---
+++ src/main/java/dt/DementiaTestApiApp.java
@@ -0,0 +1,13 @@
+package dt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DementiaTestApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DementiaTestApiApp.class, args);
+    }
+
+}
